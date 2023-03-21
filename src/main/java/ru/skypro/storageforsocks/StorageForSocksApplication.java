a0369da42eaca0606db3efd2829195c88ef05204
noheroo <serega1552@yandex.ru>
2023-03-22 01:00:46 +0400

    Initial commit

---
+++ src/main/java/ru/skypro/storageforsocks/StorageForSocksApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.storageforsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageForSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageForSocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/storageforsocks/StorageForSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.storageforsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageForSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
